f90a39a8b2f94b95b81cb59d1406573d741f0f7b
Oliver Marriott <hello@omarriott.com>
2020-11-18 11:24:30 +1100

    Add TS and LSP groups to sample spec

--- sample_lush_spec.lua
+++ sample_lush_spec.lua
@@ -279,7 +279,9 @@ local theme = lush(function()
     -- SpecialComment { }, -- special things inside a comment
     -- Debug          { }, --    debugging statements
 
-    -- Underlined     { }, -- (preferred) text that stands out, HTML links
+    -- Underlined { gui = "underline" }, -- (preferred) text that stands out, HTML links
+    -- Bold       { gui = "bold" },
+    -- Italic     { gui = "italic" },
 
     -- ("Ignore", below, may be invisible...)
     -- Ignore         { }, -- (preferred) left blank, hidden  |hl-Ignore|
@@ -287,6 +289,76 @@ local theme = lush(function()
     -- Error          { }, -- (preferred) any erroneous construct
 
     -- Todo           { }, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
+    -- These groups are for the native LSP client. Some other LSP clients may use
+    -- these groups, or use their own. Consult your LSP client's documentation.
+
+    -- LspDiagnosticsError               { }, -- used for "Error" diagnostic virtual text
+    -- LspDiagnosticsErrorSign           { }, -- used for "Error" diagnostic signs in sign column
+    -- LspDiagnosticsErrorFloating       { }, -- used for "Error" diagnostic messages in the diagnostics float
+    -- LspDiagnosticsWarning             { }, -- used for "Warning" diagnostic virtual text
+    -- LspDiagnosticsWarningSign         { }, -- used for "Warning" diagnostic signs in sign column
+    -- LspDiagnosticsWarningFloating     { }, -- used for "Warning" diagnostic messages in the diagnostics float
+    -- LspDiagnosticsInformation         { }, -- used for "Information" diagnostic virtual text
+    -- LspDiagnosticsInformationSign     { }, -- used for "Information" signs in sign column
+    -- LspDiagnosticsInformationFloating { }, -- used for "Information" diagnostic messages in the diagnostics float
+    -- LspDiagnosticsHint                { }, -- used for "Hint" diagnostic virtual text
+    -- LspDiagnosticsHintSign            { }, -- used for "Hint" diagnostic signs in sign column
+    -- LspDiagnosticsHintFloating        { }, -- used for "Hint" diagnostic messages in the diagnostics float
+    -- LspReferenceText                  { }, -- used for highlighting "text" references
+    -- LspReferenceRead                  { }, -- used for highlighting "read" references
+    -- LspReferenceWrite                 { }, -- used for highlighting "write" references
+
+    -- These groups are for the neovim tree-sitter highlights.
+    -- As of writing, tree-sitter support is a WIP, group names may change.
+    -- By default, most of these groups link to an appropriate Vim group,
+    -- TSError -> Error for example, so you do not have to define these unless
+    -- you explicitly want to support Treesitter's improved syntax awareness.
+
+    -- TSError              { }, -- For syntax/parser errors.
+    -- TSPunctDelimiter     { }, -- For delimiters ie: `.`
+    -- TSPunctBracket       { }, -- For brackets and parens.
+    -- TSPunctSpecial       { }, -- For special punctutation that does not fall in the catagories before.
+    -- TSConstant           { }, -- For constants
+    -- TSConstBuiltin       { }, -- For constant that are built in the language: `nil` in Lua.
+    -- TSConstMacro         { }, -- For constants that are defined by macros: `NULL` in C.
+    -- TSString             { }, -- For strings.
+    -- TSStringRegex        { }, -- For regexes.
+    -- TSStringEscape       { }, -- For escape characters within a string.
+    -- TSCharacter          { }, -- For characters.
+    -- TSNumber             { }, -- For integers.
+    -- TSBoolean            { }, -- For booleans.
+    -- TSFloat              { }, -- For floats.
+    -- TSFunction           { }, -- For function (calls and definitions).
+    -- TSFuncBuiltin        { }, -- For builtin functions: `table.insert` in Lua.
+    -- TSFuncMacro          { }, -- For macro defined fuctions (calls and definitions): each `macro_rules` in Rust.
+    -- TSParameter          { }, -- For parameters of a function.
+    -- TSParameterReference { }, -- For references to parameters of a function.
+    -- TSMethod             { }, -- For method calls and definitions.
+    -- TSField              { }, -- For fields.
+    -- TSProperty           { }, -- Same as `TSField`.
+    -- TSConstructor        { }, -- For constructor calls and definitions: `                                                                       { }` in Lua, and Java constructors.
+    -- TSConditional        { }, -- For keywords related to conditionnals.
+    -- TSRepeat             { }, -- For keywords related to loops.
+    -- TSLabel              { }, -- For labels: `label:` in C and `:label:` in Lua.
+    -- TSOperator           { }, -- For any operator: `+`, but also `->` and `*` in C.
+    -- TSKeyword            { }, -- For keywords that don't fall in previous categories.
+    -- TSKeywordFunction    { }, -- For keywords used to define a fuction.
+    -- TSException          { }, -- For exception related keywords.
+    -- TSType               { }, -- For types.
+    -- TSTypeBuiltin        { }, -- For builtin types (you guessed it, right ?).
+    -- TSNamespace          { }, -- For identifiers referring to modules and namespaces.
+    -- TSInclude            { }, -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
+    -- TSAnnotation         { }, -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
+    -- TSText               { }, -- For strings considered text in a markup language.
+    -- TSStrong             { }, -- For text to be represented with strong.
+    -- TSEmphasis           { }, -- For text to be represented with emphasis.
+    -- TSUnderline          { }, -- For text to be represented with an underline.
+    -- TSTitle              { }, -- Text that is part of a title.
+    -- TSLiteral            { }, -- Literal text.
+    -- TSURI                { }, -- Any URI like a link or email.
+    -- TSVariable           { }, -- Any variable name that does not have another highlight.
+    -- TSVariableBuiltin    { }, -- Variable names that are defined by the languages, like `this` or `self`.
   }
 end)
 
