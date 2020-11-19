bd6213b14aa7d85f3a7b21f7f8a675a8c807d965
Oliver Marriott <hello@omarriott.com>
2020-11-19 20:05:56 +1100

    Add example-example to examples

---
+++ examples/.example-example/lua/lush_theme/example_example.lua
@@ -0,0 +1,26 @@
+--
+-- Built with,
+--
+--        ,gggg,
+--       d8" "8I                         ,dPYb,
+--       88  ,dP                         IP'`Yb
+--    8888888P"                          I8  8I
+--       88                              I8  8'
+--       88        gg      gg    ,g,     I8 dPgg,
+--  ,aa,_88        I8      8I   ,8'8,    I8dP" "8I
+-- dP" "88P        I8,    ,8I  ,8'  Yb   I8P    I8
+-- Yb,_,d88b,,_   ,d8b,  ,d8b,,8'_   8) ,d8     I8,
+--  "Y8P"  "Y888888P'"Y88P"`Y8P' "YY8P8P88P     `Y8
+--
+
+local lush = require('lush')
+local hsl = lush.hsl
+
+local theme = lush(function()
+  return {
+    Normal { fg = hsl(100, 50, 50) },
+  }
+end)
+
+-- return our parsed theme for extension or use else where.
+return theme
