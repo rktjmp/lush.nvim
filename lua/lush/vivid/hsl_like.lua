75d6083645f6305622d1926d6b546f8ed02a8d48
Oliver Marriott <hello@omarriott.com>
2021-06-27 04:46:21 +1000

    Refactor hsl_like to be nicer

--- lua/lush/vivid/hsl_like.lua
+++ lua/lush/vivid/hsl_like.lua
@@ -2,205 +2,233 @@ local clamp = require('lush.math').clamp
 local round = require('lush.math').round
 
 --
--- HSL-like colours
+-- HSL-like colour functions
 --
--- We support both HSL and HSLUV colourspaces, which behave
--- the same except when they are converting out into RGB
--- so this module allows us to unify code for both color models.
+-- Vivid supports both HSL and HSLUV colourspaces, which functionally
+-- behave the same except when they are converting out into RGB.
+--
+-- This module provides common operations on hsl-like colours.
 --
 
 local function hsl_clamp(color)
   local h, s, l
-  h = color.h % 360
+  h = round(color.h % 360)
   s = round(clamp(color.s, 0, 100))
   l = round(clamp(color.l, 0, 100))
   return { h = h, s = s, l = l }
 end
 
-local function wrap_color(color, colorspace_to_hex_fn)
-  if not colorspace_to_hex_fn then error("Must specify colorspace_to_hex_fn") end
+-- (color, key) -> (value) -> {h, s, l}
+-- Given a color and key (h, s or l), returns a function which accepts a value which will
+-- return a new color with the key set to given value
+local function make_abs_fn(color, key)
+  return function(abs_value)
+    if type(abs_value) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    local new_values = {h = color.h, s = color.s, l = color.l}
+    new_values[key] = new_values[key] + abs_value
+    return new_values
+  end
+end
+
+-- (color, key) -> (value) -> {h, s, l}
+-- Given a color and key (h, s or l), returns a function which accepts a value which will
+-- return a new color with the key lerped by given value
+local function make_lerp_fn(color, key)
+  return function(percent)
+    if type(percent) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+
+    -- we never modifiy the caller
+    local new_values = {h = color.h, s = color.s, l = color.l}
+    -- we can safely bounds all relative adjustments to 0, 100
+    -- because you can't 'relatively rotate' hue
+    local min, max = 0, 100
+    -- we want to lerp between the current value, and the potential largest
+    -- change for -percent this is [0, current], +percent, [0, max - current]
+    local lerp_space = percent < min and new_values[key] or (max - new_values[key])
+    -- perform the lerp
+    new_values[key] = new_values[key] + (lerp_space * (percent / 100))
+    return new_values
+  end
+end
+
+-- (color) -> (n) -> {h + n, s, l}
+local function op_rotate(color)
+  return make_abs_fn(color, "h")
+end
+
+-- (color) -> (n) -> {h, s lerp n, l}
+local function op_saturate(color)
+  return make_lerp_fn(color, "s")
+end
+
+-- (color) -> (n) -> {h, s + n, l}
+local function op_abs_saturate(color)
+  return make_abs_fn(color, "s")
+end
+
+-- (color) -> (n) -> {h, s lerp -n, l}
+local function op_desaturate(color)
+  return function(percent)
+    if type(percent) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return make_lerp_fn(color, "s")(-percent)
+  end
+end
+
+-- (color) -> (n) -> {h, s - n, l}
+local function op_abs_desaturate(color)
+  return function(abs_value)
+    if type(abs_value) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return make_abs_fn(color, "s")(-abs_value)
+  end
+end
+
+-- (color) -> (n) -> {h, s, l lerp n}
+local function op_lighten(color)
+  return make_lerp_fn(color, "l")
+end
+
+-- (color) -> (n) -> {h, s, l + n}
+local function op_abs_lighten(color)
+  return make_abs_fn(color, "l")
+end
+
+-- (color) -> (n) -> {h, s, l lerp -n}
+local function op_darken(color)
+  return function(percent)
+    if type(percent) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return make_lerp_fn(color, "l")(-percent)
+  end
+end
+
+-- (color) -> (n) -> {h, s, l - n}
+local function op_abs_darken(color)
+  return function(abs_value)
+    if type(abs_value) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return make_abs_fn(color, "l")(-abs_value)
+  end
+end
+
+-- mix ref:
+-- https://stackoverflow.com/questions/35816179/calculation-algorithm-to-mix-3-hsl-colors
+-- (color) -> (color, n) -> {h x h x n, s x s x n, l x l x n}
+local function op_mix(color)
+  return function(target, strength)
+    assert(strength, "must provide strength to mix")
+    if target == 'black' then
+      target = {h = color.h, s = color.s, l = 0}
+    end
+    if target == 'white' then
+      target = {h = color.h, s = color.s, l = 100}
+    end
+
+    strength = clamp(strength, 0, 100) / 100
+    -- strength of 0 means no mix towards target, so
+    -- color vector strength is 1
+    local cv_str = (1 - strength)
+    -- target strength is the remainder, so
+    -- str = 0, cv_str = 1, tv_str = 0
+    -- str = 100, cv_str = 0, tv_str = 1
+    local tv_str = 1 - cv_str
+
+    -- convert colors to vector
+    local cv = {
+      x = math.cos(color.h / 180 * math.pi) * color.s,
+      y = math.sin(color.h / 180 * math.pi) * color.s,
+      z = color.l
+    }
+    local tv = {
+      x = math.cos(target.h / 180 * math.pi) * target.s,
+      y = math.sin(target.h / 180 * math.pi) * target.s,
+      z = target.l
+    }
+    -- combine
+    local rv = {
+      x = ((cv.x * cv_str) + (tv.x * tv_str)) / 1,
+      y = ((cv.y * cv_str) + (tv.y * tv_str)) / 1,
+      z = ((cv.z * cv_str) + (tv.z * tv_str)) / 1,
+    }
+    -- back to color
+    local new_values = {
+      h = math.atan2(rv.y, rv.x) * (180 / math.pi),
+      s = math.sqrt(rv.x * rv.x + rv.y * rv.y),
+      l = rv.z
+    }
+    return new_values
+  end
+end
+
+-- (color) -> (n) -> {n, s, l}
+local function op_hue(color)
+  return function(hue)
+    if type(hue) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return {h = hue, s = color.s, l = color.l}
+  end
+end
+
+-- (color) -> (n) -> {h, n, l}
+local function op_saturation(color)
+  return function(saturation)
+    if type(saturation) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return {h = color.h, s = saturation, l = color.l}
+  end
+end
+
+-- (color) -> (n) -> {h, s, n}
+local function op_lightness(color)
+  return function(lightness)
+    if type(lightness) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    return {h = color.h, s = color.s, l = lightness}
+  end
+end
+
+-- (color) -> (n) -> {h, s, 0 | 100}
+local function op_readable(color)
+  return function()
+    if color.l >= 50 then
+      return {h = color.h, s = color.s, l = 0}
+    else
+      return {h = color.h, s = color.s, l = 100}
+    end
+  end
+end
+
+-- define as var for self-recursion in op application
+local function make_color(color, to_hex_fn)
+  if not to_hex_fn then error("Must specify to_hex_fn") end
 
   -- make sure our color is valid
   color = hsl_clamp(color)
 
-  local roll_abs_fn = function(color, key)
-    return function(abs_value)
-      if type(abs_value) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      local new_color = {h = color.h, s = color.s, l = color.l}
-      new_color[key] = new_color[key] + abs_value
-      return wrap_color(new_color, colorspace_to_hex_fn)
-    end
-  end
+  local op_fns = {
+    rotate = op_rotate,
+    ro = op_rotate,
 
-  local roll_lerp_fn = function(color, key)
-    return function(percent)
-      if type(percent) ~= "number" then error("Must provide number to HSL modifiers", 0) end
+    saturate = op_saturate,
+    sa = op_saturate,
+    abs_saturate = op_abs_saturate,
+    abs_sa = op_abs_saturate,
 
-      -- we never modifiy the caller
-      local new_color = {h = color.h, s = color.s, l = color.l}
-      -- we can safely bounds all relative adjustments to 0, 100
-      -- because you can't 'relatively rotate' hue
-      local min, max = 0, 100
-      -- we want to lerp between the current value, and the potential largest
-      -- change for -percent this is [0, current], +percent, [0, max - current]
-      local lerp_space = percent < 0 and new_color[key] or (max - new_color[key])
-      -- perform the lerp
-      new_color[key] = new_color[key] + (lerp_space * (percent / 100))
-      return wrap_color(new_color, colorspace_to_hex_fn)
-    end
-  end
+    desaturate = op_desaturate,
+    de = op_desaturate,
+    abs_desaturate = op_abs_desaturate,
+    abs_de = op_abs_desaturate,
 
-  local rotate = function(color)
-    return roll_abs_fn(color, "h")
-  end
+    lighten = op_lighten,
+    li = op_lighten,
+    abs_lighten = op_abs_lighten,
+    abs_li = op_abs_lighten,
 
-  local saturate = function(color)
-    return roll_lerp_fn(color, "s")
-  end
-  local abs_saturate = function(color)
-    return roll_abs_fn(color, "s")
-  end
+    darken = op_darken,
+    da = op_darken,
+    abs_darken = op_abs_darken,
+    abs_da = op_abs_darken,
 
-  local desaturate = function(color)
-    return function(percent)
-      if type(percent) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return roll_lerp_fn(color, "s")(-percent)
-    end
-  end
-  local abs_desaturate = function(color)
-    return function(abs_value)
-      if type(abs_value) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return roll_abs_fn(color, "s")(-abs_value)
-    end
-  end
+    mix = op_mix,
+    readable = op_readable,
 
-  local lighten = function(color)
-    return roll_lerp_fn(color, "l")
-  end
-  local abs_lighten = function(color)
-    return roll_abs_fn(color, "l")
-  end
-
-  local darken = function(color)
-    return function(percent)
-      if type(percent) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return roll_lerp_fn(color, "l")(-percent)
-    end
-  end
-  local abs_darken = function(color)
-    return function(abs_value)
-      if type(abs_value) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return roll_abs_fn(color, "l")(-abs_value)
-    end
-  end
-
-  -- mix ref:
-  -- https://stackoverflow.com/questions/35816179/calculation-algorithm-to-mix-3-hsl-colors
-  local mix = function(color)
-    return function(target, strength)
-      assert(strength, "must provide strength to mix")
-      if target == 'black' then
-        target = {h = color.h, s = color.s, l = 0}
-      end
-      if target == 'white' then
-        target = {h = color.h, s = color.s, l = 100}
-      end
-
-      strength = clamp(strength, 0, 100) / 100
-      -- strength of 0 means no mix towards target, so
-      -- color vector strength is 1
-      local cv_str = (1 - strength)
-      -- target strength is the remainder, so
-      -- str = 0, cv_str = 1, tv_str = 0
-      -- str = 100, cv_str = 0, tv_str = 1
-      local tv_str = 1 - cv_str
-
-      -- convert colors to vector
-      local cv = {
-        x = math.cos(color.h / 180 * math.pi) * color.s,
-        y = math.sin(color.h / 180 * math.pi) * color.s,
-        z = color.l
-      }
-      local tv = {
-        x = math.cos(target.h / 180 * math.pi) * target.s,
-        y = math.sin(target.h / 180 * math.pi) * target.s,
-        z = target.l
-      }
-      -- combine
-      local rv = {
-        x = ((cv.x * cv_str) + (tv.x * tv_str)) / 1,
-        y = ((cv.y * cv_str) + (tv.y * tv_str)) / 1,
-        z = ((cv.z * cv_str) + (tv.z * tv_str)) / 1,
-      }
-      -- back to color
-      local new_color = {
-        h = math.atan2(rv.y, rv.x) * (180 / math.pi),
-        s = math.sqrt(rv.x * rv.x + rv.y * rv.y),
-        l = rv.z
-      }
-      return wrap_color(new_color, colorspace_to_hex_fn)
-    end
-  end
-
-  local hue = function(color)
-    return function(hue)
-      if type(hue) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return wrap_color({h = hue, s = color.s, l = color.l}, colorspace_to_hex_fn)
-    end
-  end
-  local saturation = function(color)
-    return function(saturation)
-      if type(saturation) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return wrap_color({h = color.h, s = saturation, l = color.l}, colorspace_to_hex_fn)
-    end
-  end
-  local lightness = function(color)
-    return function(lightness)
-      if type(lightness) ~= "number" then error("Must provide number to HSL modifiers", 0) end
-      return wrap_color({h = color.h, s = color.s, l = lightness}, colorspace_to_hex_fn)
-    end
-  end
-
-  local readable = function(color)
-    return function()
-      if color.l >= 50 then
-        return wrap_color({h = color.h, s = color.s, l = 0}, colorspace_to_hex_fn)
-      else
-        return wrap_color({h = color.h, s = color.s, l = 100}, colorspace_to_hex_fn)
-      end
-    end
-  end
-
-  local mod_fns = {
-    rotate = rotate,
-    ro = rotate,
-
-    saturate = saturate,
-    sa = saturate,
-    abs_saturate = abs_saturate,
-    abs_sa = abs_saturate,
-
-    desaturate = desaturate,
-    de = desaturate,
-    abs_desaturate = abs_desaturate,
-    abs_de = abs_desaturate,
-
-    lighten = lighten,
-    li = lighten,
-    abs_lighten = abs_lighten,
-    abs_li = abs_lighten,
-
-    darken = darken,
-    da = darken,
-    abs_darken = abs_darken,
-    abs_da = abs_darken,
-
-    mix = mix,
-    readable = readable,
-
-    hue = hue,
-    saturation = saturation,
-    lightness = lightness
+    hue = op_hue,
+    saturation = op_saturation,
+    lightness = op_lightness,
   }
 
   return setmetatable({}, {
@@ -209,30 +237,36 @@ local function wrap_color(color, colorspace_to_hex_fn)
       if key_name == "h" then return color.h end
       if key_name == "s" then return color.s end
       if key_name == "l" then return color.l end
-      if key_name == "hex" then return colorspace_to_hex_fn(color) end
+      if key_name == "hex" then return to_hex_fn(color) end
       if key_name == "hsl" then return {h = color.h, s = color.s, l = color.l} end
 
-      if mod_fns[key_name] then
-        return mod_fns[key_name](color)
+      -- look up requested key in operations table and call out
+      -- if it exists, else try to show a nice warning.
+      if op_fns[key_name] then
+        return function(...)
+          local altered_color = op_fns[key_name](color)(...)
+          return make_color(altered_color, to_hex_fn)
+        end
       else
         local ops = ""
-        for op, _ in pairs(mod_fns) do
+        for op, _ in pairs(op_fns) do
           ops = ops .. " " .. op
         end
+        ops = ops .. " h s l hex hsl"
         error("Invalid hsl operation: '"
-              .. key_name
-              .. "', valid operations:"
-              .. ops, 2)
+        .. key_name
+        .. "', valid operations:"
+        .. ops, 2)
       end
     end,
 
     -- possibly this won't be useless, but for now disable
-    __newindex = function(table, key, value)
+    __newindex = function(_, _, _)
       error('Member setting disabled', 2)
     end,
 
     __tostring = function(hsl)
-      return colorspace_to_hex_fn(hsl)
+      return to_hex_fn(hsl)
     end,
 
     __concat = function(lhs, rhs)
@@ -246,4 +280,8 @@ local function wrap_color(color, colorspace_to_hex_fn)
   })
 end
 
-return wrap_color
+local M = function(hsl, hex_fn)
+  return make_color(hsl, hex_fn)
+end
+
+return M
