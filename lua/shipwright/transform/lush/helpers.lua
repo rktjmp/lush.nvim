0517452a74fe25f6a62cacb75e4b25bb1dcb2803
Oliver Marriott <hello@omarriott.com>
2022-05-12 15:02:14 +1000

    Sort Normal to top of list in to_lua shipwright transform

--- lua/shipwright/transform/lush/helpers.lua
+++ lua/shipwright/transform/lush/helpers.lua
@@ -8,8 +8,28 @@ local function is_lush_spec(spec)
   end
 end
 
+local sort_value = function(group, attrs)
+  -- Normal group must appear first as other groups may implicity use its
+  -- values via fg = "bg"
+  local normal_special = "000Normal"
+  if attrs.link then
+    if attrs.link == "Normal" then
+      return normal_special .. "1" .. group
+    else
+      return attrs.link .. "1" .. group
+    end
+  else
+    if group == "Normal" then
+      return normal_special
+    else
+      return group
+    end
+  end
+end
+
 
 return {
   -- is argument a lush spec
   is_lush_spec = is_lush_spec,
+  group_sort_value = sort_value
 }
